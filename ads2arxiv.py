b05a2332bc183727583a6ef93a1090d90e4c091e
Geert Barentsen <geert@barentsen.be>
2019-05-02 11:41:38 -0700

    Bug fix!

--- ads2arxiv.py
+++ ads2arxiv.py
@@ -3,20 +3,22 @@
 def get_arxiv_ids(ads_query="full:'github.com' Kepler"):
     """Returns arXiv identifiers of papers given an ADS query.
 
-    For this to work, you need to `pip install ads` and also make sure
-    to store your ADS developer key in `~/.ads/dev_key `.
-    You can get an ADS key from https://ui.adsabs.harvard.edu/user/settings/token.
+    For this to work, you need to do two things:
+    1. `pip install ads`
+    2. Get a ADS developer key from https://ui.adsabs.harvard.edu/user/settings/token
+    and store it in `~/.ads/dev_key `.
     """
     import ads
     out_identifiers, out_citations = [], []
     qry = ads.SearchQuery(q=ads_query,
                         fl=['identifier', 'citation_count'],
-                        rows=9999999999)
+                        rows=9999999999)  # the default is something low
     for paper in qry:
         for identifier in paper.identifier:
             if 'arXiv:' in identifier:
                 out_identifiers.append(identifier)
                 out_citations.append(paper.citation_count)
+    return out_identifiers, out_citations
 
 
 if __name__ == "__main__":

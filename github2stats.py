3a0d4c6057e24da96fbdd1c8596a4628721f299f
Geert Barentsen <geert@barentsen.be>
2019-05-02 14:14:17 -0700

    First commit of GitHub stats script

---
+++ github2stats.py
@@ -0,0 +1,73 @@
+"""Retrieve statistics on a GitHub repository."""
+import os
+import requests
+
+TOKEN = open(os.path.expanduser("~/.github/token")).read().strip()
+HEADERS = {"Authorization": f"Bearer {TOKEN}"}
+
+def query_github(query):
+    """Query the GitHub API."""
+    request = requests.post('https://api.github.com/graphql',
+                            json={'query': query},
+                            headers=HEADERS)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}"
+                        "".format(request.status_code, query))
+
+
+def github2stats(repository_owner="keplergo", repository_name="lightkurve"):
+    """Retrieves GitHub stats given a GitHub username and repository."""
+    query = """
+        query {
+            repository(owner:"%s", name:"%s") {
+                createdAt
+            pushedAt
+            shortDescriptionHTML
+            parent {
+                id
+            }
+            primaryLanguage {
+                name
+            }
+            licenseInfo {
+                        name
+                spdxId
+                pseudoLicense
+            },
+            forkCount,
+            collaborators(first:1) {
+                totalCount
+                edges {
+                node {
+                    id
+                }
+                }
+            }
+            }
+    }""" % (repository_owner, repository_name)
+    result = query_github(query)
+    return result
+
+
+def get_rate_limit():
+    query = """
+    {
+    viewer {
+        login
+    }
+    rateLimit {
+        limit
+        cost
+        remaining
+        resetAt
+    }
+    }
+    """
+    return query_github(query)
+
+
+if __name__ == "__main__": 
+    print(github2stats())     
+    print(get_rate_limit())
